f1485b04e5dd65e7f435427287c13ac89043c5f7
Diachenko Sergey <diachenko.webnauts@gmail.com>
2021-10-15 17:41:17 +0300

    begining third hw

---
+++ 03 - third lesson/src/components/MyInput.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="form-group my-input">
+		<label>{{ this.name }}</label>
+		<input type="text" class="form-control" :class="isError">
+	</div>
+</template>
+
+<script>
+
+
+export default {
+    props: ['name', 'isRight'],
+    computed: {
+        isError() {
+            switch (this.isRight) {
+                case 0: return ''
+                case 1: return 'input-green'
+                case 2: return 'input-red'
+            }
+            return ''
+        }
+    },
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+ .my-input {
+    width: 50%; 
+    margin-left: 25%;
+    margin-top: 10px;
+ }
+
+ .input-red {
+     border-color: red;
+ }
+
+ .input-green {
+     border-color: green;
+ }
+
+</style>
